b80d1bbcbe13f2720123d827348f9b5aa5ac484a
Mohamed essamaali <mohamed.essamaali@gmail.com>
2020-09-03 21:25:23 -0400

    set up project

---
+++ team-builder/src/components/user.js
@@ -0,0 +1,11 @@
+import React ,{useState} from 'react'
+
+const User = props=>{
+
+    return (
+        <div className='user'>
+            <h1>user</h1>
+        </div>
+    )
+}
+export default User
---
+++ team-builder/src/components/userForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UserForm = props=>{
+
+    return (
+        <form>
+<h1>Form</h1>
+
+        </form>
+    )
+}
+export default UserForm
